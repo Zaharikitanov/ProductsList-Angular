ded31e69793fb811a2c978f6d80513e5ab648ddd
Zahari Kitanov <Zahari.Kitanov@stp.ag>
2020-07-30 17:56:19 +0300

    Products list added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+    AppComponent, 
+    ProductListComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
